8541bb8f9a586a35e117d1963a16497d911a7d1b
v02r <vmprio@yandex.ru>
2024-02-25 20:09:07 +0300

    fixed redirect when connects to protected page without auth

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children, isAuth, loading }) => {
 
     if (!loading && !isAuth) {
-        return <Navigate to="/signin" replace />;
+        return <Navigate to="/" replace />;
     }
 
     return loading ? null : (
